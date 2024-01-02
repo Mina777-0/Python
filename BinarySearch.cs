0a6723755a921f4f19f203a8bc4060a16ffa75f7
Mina Eskander <127833856+Mina777-0@users.noreply.github.com>
2024-01-02 12:38:32 +0200

    Add files via upload

---
+++ BinarySearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Myapp
+{
+    class Programme
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("enter the number: ");
+            int number = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("enter the min number: ");
+            int min = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("enter the max numebr: ");
+            int max = Convert.ToInt16(Console.ReadLine());
+
+            Binary(number, min, max);
+        }
+
+        static void Binary(int number, int min, int max)
+        {
+            if (min != max && min < max && number > min && number < max)
+            {
+                int mean = (min + max) / 2;
+
+                if (mean == number)
+                {
+                    Console.WriteLine("Found");
+                }
+
+                else if (mean < number)
+                {
+                    Binary(number, mean, max);
+                }
+
+                else
+                {
+                    Binary(number, min, mean);
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("There is something wrong");
+            }
+        }
+    }
+}
